e93487328522006a72c42ed0905ff2225e76be03
omayib <omayib@gmail.com>
2016-02-08 22:44:41 +0700

    screenshot login.png

--- app/src/main/java/id/technomotion/todolist/LoginActivity.java
+++ app/src/main/java/id/technomotion/todolist/LoginActivity.java
@@ -1,8 +1,12 @@
 package id.technomotion.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +25,36 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupView();
+        setupViewListener();
+    }
+
+    private void setupViewListener() {
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //any code here...
+                processLogin();
+            }
+        });
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //any code here...
+            }
+        });
+    }
+
+    private void processLogin() {
+        String email=editTextEmail.getText().toString();
+        String password=editTextPassword.getText().toString();
+
+        if(email.equalsIgnoreCase("coba@email.com") && password.equalsIgnoreCase("1234")){
+            Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+        }else{
+            Snackbar.make(buttonLogin,"login failed",Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     private void setupView() {
--- app/src/main/java/id/technomotion/todolist/MainActivity.java
+++ app/src/main/java/id/technomotion/todolist/MainActivity.java
@@ -14,50 +14,16 @@ public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fab;
     ListView listView;
-    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setupView();
-
     }
 
     private void setupView() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         listView= (ListView) findViewById(R.id.listview);
-
-        setSupportActionBar(toolbar);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
